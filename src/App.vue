bc9a750eb0ba85c5b196496d5ddc682a6127e5d9
Moises Linares O <elnaufrago2009@hotmail.com>
2020-05-14 09:32:00 -0500

    heigh no found

--- src/App.vue
+++ src/App.vue
@@ -1,30 +1,6 @@
 <template>
   <div id="app">    
     <Navbar v-if="layout=='admin'"></Navbar>
-
-    <!-- Mega Menu -->
-    <!--<div class="relative">
-        <a href class>Megamenu</a>
-        <div
-          class="absolute normal-case font-normal bg-white shadow-md rounded-lg overflow-hidden border mt-4" style=" left: -60px"
-        >
-          <div class="flex px-8 py-6 border-b -mx-4">
-            <div class="w-1/2 px-4">
-              lorem ipsum
-            </div>
-            <div class="w-1/2 px-4">
-              lorem ipsum
-            </div>
-          </div>
-          <ul class=" bg-gray-100 px-8 py-8">
-            <li>Lorem Ipsum</li>
-            <li>Lorem Ipsum</li>
-            <li>Lorem Ipsum</li>
-            <li>Lorem Ipsum</li>
-          </ul>
-
-        </div>
-    </div>-->
     <router-view />
   </div>
 </template>
@@ -39,7 +15,7 @@ export default {
   },
   methods: {
     GetCurrentRoute(){
-      if(this.$router.currentRoute.path == '/' || this.$router.currentRoute.path == '/curve' || this.$router.currentoute.path == '/home'){
+      if(this.$router.currentRoute.path == '/' || this.$router.currentRoute.path == '/curve' || this.$router.currentRoute.path == '/home'){
         this.layout = '';
       }else{
         this.layout = 'admin';
--- src/views/home/home.vue
+++ src/views/home/home.vue
@@ -1,18 +1,21 @@
 <template>
-  <div class="bg-gray-100 h-screen">
+  <div class="">
     <!-- Menu Header -->
     <nav class="py-8">
-      <div class="container mx-auto flex justify-between justify-center">        
+      <div class="mx-auto flex justify-between justify-center  max-w-screen-lg">        
 
         <!-- Logo -->
-        <router-link to="/">
-          <img class="h-8 w-auto sm:h-10 -mt-2" src="https://plausible.io/assets/images/icon/plausible_logo.compressed.png" alt="Plausible logo">
+        <router-link to="/" class=" text-2xl text-gray-700">
+          Lineysoft <span class=" text-sm">.com</span>
+          <!-- <img class="h-8 w-auto sm:h-10 -mt-2" src="https://plausible.io/assets/images/icon/plausible_logo.compressed.png" alt="Plausible logo"> -->
         </router-link>
 
         <!-- Menu Medio -->
         <div>
-          <router-link to="/" class=" font-medium text-gray-600 hover:text-gray-900">Documentos</router-link>
-          <router-link to="/" class="ml-8 font-medium text-gray-600 hover:text-gray-900">Documentos</router-link>
+          <router-link to="/" class=" font-medium text-gray-600 hover:text-gray-900">Documentacion</router-link>
+          <router-link to="/" class="ml-8 font-medium text-gray-600 hover:text-gray-900">Precios</router-link>
+          <router-link to="/" class="ml-8 font-medium text-gray-600 hover:text-gray-900">Blog</router-link>
+          <router-link to="/" class="ml-8 font-medium text-gray-600 hover:text-gray-900">Contacto</router-link>
         </div>
 
         <!-- Menu Derecho -->
@@ -32,8 +35,48 @@
     </nav>
     <!-- End Menu Header -->
 
-    <div class="pb-12">
-      Simple and privacy-friendly alternative to Google Analytics
+    <div class="mt-10 mx-auto max-w-screen-lg pt-20">
+      <h1 class=" text-center text-3xl tracking-tight leading-10 font-extrabold text-gray-900 sm:text-4xl sm:leading-none md:text-5xl">
+        Facturacion Electronica 
+        <span class=" text-indigo-700">simple y sensillo para tu empresa</span>
+      </h1> 
+      <div class=" text-xl text-gray-600 mt-5 text-center px-32">
+        Plausible is a lightweight and open-source website analytics tool. It doesn’t use cookies and is fully compliant with GDPR, CCPA and PECR.
+      </div>
+      <div class="text-center mt-10">
+        <router-link to="/" class=" rounded-md shadow px-8 py-4 text-xl leading-6 text-white bg-indigo-700 font-medium">Get started</router-link>
+        <router-link to="/" class="ml-3 rounded-md shadow bg-white px-8 py-4 text-indigo-700 text-xl">Live Demo</router-link>
+      </div>
+
+
+      
     </div>
+    <div class="">
+        <img src="@/assets/header.png" class=" h-600">
+        mas
+        <br>
+        <br>
+        <br>
+        <br>
+        <br>
+        <br>
+        dad
+        <br>
+        <br>
+        <br>
+        <br>
+        <br>
+        <br>
+        dsaddsaddas
+        ds
+
+      </div>
   </div>
 </template>
+
+<script>
+// import image from '../../../src/assets/header.png'
+export default {
+  
+}
+</script>  
